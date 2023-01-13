69c045c5c5594e1ce5d86c3dea715e5609065ea6
Fernando Ayasta <fernandoayasta25@gmail.com>
2023-01-13 10:35:49 -0500

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # karate-api
 Karate Started Project for api testing
+
+
+Karate requires JAVA 8 or greater.
+
+1. Install JAVA
+Link to download Java 8 https://www.oracle.com/java/technologies/downloads/#java8-windows
+
+2. Maven:  https://maven.apache.org/download.cgi
+Download the binary zip archive.
+
+3. Set environnment variables for JAVA and MAVEN
+Follow this tutorial: https://www.tutorialspoint.com/maven/maven_environment_setup.htm
+
+4. Download Visual Studio Code
+
+5. Open the project
+
+6. Execute the command: ```mvn test```
+
+7. See reports at:
+Karate default report: /target/karate-reports/karate-summary.html
+Cucumber Report: /target/cucumber-html-reports/overview-features.html
--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -16,8 +16,7 @@ function fn() {
   }
 
   // Insert manually the token generated after a validation.
-  config.captchaToken = "03AD1IbLBIqlIAsW5BtyG9yq_VsBpo5Y5IAA2fIWgKwHDjSk6KYGAdM27coILuS2AoWL7Pfl1sxKfABZqIGzXiJ8qbxHC279kfGLblWdw2ZPHUmG7Z34MLiyH5McCXTQjwbD2SPNYia4IoafQid3QNI0fF5IqrtCX2dP5fCj-ow0_IFUFFkaR705HR3cj7zvPGIi5mS8kezwLEwmNtwX7BuNlQvKOEJsDO4LVeAfB5Eto3SfvI2MeyS5pJVYmkSJh_EwgsE7M_OTYLuW6osPM2HhNaXV9QZakj3eKR91JtNDSty2NzlNiOmeX57rlyKcEhGIjllmzvPHUKAe0R3yRv02Ht2KNpPGiSIwR762_MpafeOWiKbYaagQ16_pnUffkFiMhbkZOrMdrjPCvZ9gPonBITeQXIWz7CXh30oBnSGejIW_COjz92tiS2wRr4GfI4ZZqArdqFbaK6ue54JcHVr6eaMPBu8-Ayh8Hug5O_ggpwKe9TFLYWrWPKT86B_qtJJHs3gokIswjJUJllMPO33Qzi-8m30QdSjOcM2QGLWKb80JyiacXSwgY"
-
+  config.captchaToken = "CAPTCHA_VERIFY_TOKEN"
   var accessToken = karate.callSingle('classpath:helpers/createToken.feature', config).access_token
   var applicationToken = karate.callSingle('classpath:helpers/createToken.feature', config).application_token
   karate.configure('headers',{application_token: applicationToken, Authorization: 'Bearer ' + accessToken, 'Ocp-Apim-Subscription-Key': "bf5c9e1a285741268a8574a8481b8a0c" })
